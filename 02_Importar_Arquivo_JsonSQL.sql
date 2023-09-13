80291b1ae2054aa45c614a072c0f66c5849774ad
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-09-13 10:10:46 -0300

    Update 02_Importar_Arquivo_JsonSQL.sql

--- 02_Importar_Arquivo_JsonSQL.sql
+++ 02_Importar_Arquivo_JsonSQL.sql
@@ -1,16 +1,16 @@
-/*
+/* Modelo do Arquivo Lido
 { 
   "pessoas": {
       "pessoa": [
         {
            "id": "1",
-           "nm": "Priscila Labor„o",
+           "nm": "Priscila Labor√£o",
            "profissao": "Recrutadora"
          },
          {
            "id": "2",
            "nm": "Tiragato Dakatola",
-           "profissao": "M·gico"
+           "profissao": "M√°gico"
          },
          {
            "id": "3",
@@ -24,14 +24,14 @@ use master
 go
 
 --------------------------------------------------
--- JSON => Vari·vel
+-- JSON => Vari√°vel
 --------------------------------------------------
 declare @j varchar(max) = (select bulkcolumn from openrowset (bulk 'c:\tmp\pessoas.json', single_clob) a)
 select @j as [json]
  
  
 --------------------------------------------------
--- JSON => Vari·vel => Tabela
+-- JSON => Vari√°vel => Tabela
 --------------------------------------------------
 select
     *
@@ -44,7 +44,7 @@ with (id int, nm varchar(100), profissao varchar(100)) pessoas
 --------------------------------------------------
 declare @pessoas table (id int, nm varchar(100), profissao varchar(100))
 insert into @pessoas values
-    (1, 'Priscila Labor„o', 'Recrutadora'),
+    (1, 'Priscila Labor√£o', 'Recrutadora'),
     (2, 'Tiragato Dakatola', 'Analista de TI'),
     (3, 'Ric Wyndfuck', 'Usineiro')
  
